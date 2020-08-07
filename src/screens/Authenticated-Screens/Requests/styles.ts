7169cf03daa451955c28c06f5dcee60167b9e41e
henrickJona <jhenrick41@gmail.com>
2020-08-07 11:54:15 -0300

    refactor: tela request

--- src/screens/Authenticated-Screens/Requests/styles.ts
+++ src/screens/Authenticated-Screens/Requests/styles.ts
@@ -1,9 +1,3 @@
-import {
-  widthPercentageToDP as wp,
-  heightPercentageToDP as hp,
-} from 'react-native-responsive-screen';
-import Icon from 'react-native-vector-icons/FontAwesome';
-
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
@@ -14,98 +8,88 @@ export const Container = styled.View`
 
 export const Section = styled.View`
   background: #f2f1f7;
-  height: ${hp('5%')}px;
+  height: 32px;
   align-content: center;
   justify-content: center;
 `;
 
 export const SectionTitle = styled.Text`
   color: #747479;
-  font-size: ${hp('2.4%')}px;
-  padding-left: ${hp('2.5%')}px;
+  font-size: 15px;
+  padding-left: 17px;
   font-family:${(props)=> props.theme.fonts.Ubuntu.normal }
 `;
 
-export const WrapperList = styled.View`
-  height: ${hp('30%')}px;
-  width: ${wp('85%')}px;
-`;
-
-export const ListWrapper = styled.View`
-  align-items: center;
-  background: #f2f1f7;
-  height: ${hp('63%')}px;
-`;
 export const ListWrapperOrders = styled.View`
   align-items: center;
   background: #f2f1f7;
-  height: ${hp('72.5%')}px;
+  height: 100%;
 `;
 
 export const ListRow = styled.TouchableOpacity`
   justify-content: center;
-  height: ${hp('17%')}px;
-  width: ${wp('95%')}px;
+  height: 109px;
+  width: 100%;
   border-width: 1px;
   border-color: #e4e4e6;
-  margin-bottom: ${hp('0.2%')}px;
+  margin-bottom: 2px;
   background: white;
   flex-direction:row;
 `;
 
 export const ListRowInnerLeft = styled.View`
   justify-content: center;
-  width: ${wp('45%')}px;
-  margin-bottom: ${hp('0.2%')}px;
+  width:47%;
+  margin-bottom: 2px;
   background: white;
 `;
 
 export const ListRowInnerRight = styled.View`
   justify-content: center;
-  width: ${wp('45%')}px;
-  margin-bottom: ${hp('0.2%')}px;
+  width:47%;
+  margin-bottom: 4px;
   background: white;
   align-items:flex-end;
 `;
 
 export const ListRowTitle = styled.Text`
-  font-size: ${hp('2.8%')}px;
+  font-size: 16px;
   color:#333;
   font-family:${(props)=> props.theme.fonts.Ubuntu.bold }
 `;
 
 export const ListRowSubTitle = styled.Text`
-  padding-top: ${hp('.5%')}px;
-  font-size: ${hp('2%')}px;
+  padding-top: 3px;
+  font-size: 12px;
   color: #666666;
   font-family:${(props)=> props.theme.fonts.Ubuntu.normal }
 `;
 
 export const ListRowPending = styled.Text`
   width:60px;
-  font-size: ${hp('2%')}px;
+  font-size:12px;
   color: white;
   background:#ffd500;
   text-align:center;
   border-radius:2px;
  
-  margin-bottom:${hp('3%')}px;
+  margin-bottom:18px;
   font-family:${(props)=> props.theme.fonts.Ubuntu.normal }
 `;
 
 export const ListRowConfirmed = styled.Text`
   width:80px;
-  font-size: ${hp('2%')}px;
+  font-size: 12px;
   color: white;
   background:#59A656;
   text-align:center;
   border-radius:2px;
-  margin-bottom:${hp('3%')}px;
+  margin-bottom:18px;
   font-family:${(props)=> props.theme.fonts.Ubuntu.normal }
 `;
 
 export const ListRowTotal = styled.Text`
-  font-size: ${hp('2%')}px;
+  font-size: 12px;
   color: #666666;
   font-family:${(props)=> props.theme.fonts.Ubuntu.bold }
 `;
