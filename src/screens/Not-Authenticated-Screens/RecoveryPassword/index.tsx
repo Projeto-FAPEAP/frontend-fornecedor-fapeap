dac359cc14f2e00738688f8f61a4eaf87189610e
henrickJona <jhenrick41@gmail.com>
2020-09-18 11:40:24 -0300

    refactor: tela de recuperação de senha padronizada

--- src/screens/Not-Authenticated-Screens/RecoveryPassword/index.tsx
+++ src/screens/Not-Authenticated-Screens/RecoveryPassword/index.tsx
@@ -2,49 +2,73 @@ import React from 'react';
 import { Text, SafeAreaView } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { useNavigation } from '@react-navigation/native';
-import {
-  Container,
-  Title,
-  Input,
-  LoginButton,
-  LoginButtonText,
-  RetrievePasswordButton,
-  RetrievePasswordText,
-  Form,
-  Header,
-  RegisterButton,
-  RegularText,
-  RegisterButtonText,
-  P,
-  BackButtonWrapper,
-} from './styles';
 
+import Input from '@components/Input';
+import { useNavigation } from '@react-navigation/native';
+import { FormHandles } from '@unform/core';
+import { Form as FormProvider } from '@unform/mobile';
+import getValidationErrors from '@utils/getValidationErrors';
+import * as Yup from 'yup';
+
+import { Container, Title, Form, Header, ButtonSubmit, P } from './styles';
+
+interface ISubmitForm {
+  email: string;
+}
 const RecoveryPassword: React.FC = () => {
   const navigation = useNavigation();
+  const formRef = React.useRef<FormHandles>(null);
+  const [loading, setLoading] = React.useState(false);
+  const handleSubmit = React.useCallback(async (data: ISubmitForm) => {
+    formRef.current?.setErrors({});
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string().required('E-mail é obrigatório'),
+      });
+
+      await schema.validate(data, { abortEarly: false });
+
+      const { email } = data;
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
+      }
+    }
+  }, []);
+
   return (
     <Container>
       <KeyboardAwareScrollView>
         <Header>
-          <BackButtonWrapper onPress={() => navigation.goBack()}>
-            <Icon color="#84378F" size={28} name="chevron-left" />
-          </BackButtonWrapper>
-          <Title>
-            {`Recupere sua
-conta`}
-          </Title>
+          <Title>Recupere sua{'\n'}senha</Title>
           <P>
             Por favor, insira o email da sua conta para receber o link de
             recuperação de senha.
           </P>
         </Header>
-        <Form>
-          <Input placeholder="Email associado a sua conta" />
+        <FormProvider onSubmit={handleSubmit} ref={formRef}>
+          <Form>
+            <Input
+              icon="mail"
+              label="E-mail"
+              name="email"
+              placeholder="Email associado a sua conta"
+              autoCapitalize="none"
+              returnKeyType="next"
+              containerStyle={{
+                maxWidth: 350,
+              }}
+            />
 
-          <LoginButton>
-            <LoginButtonText>Recuperar Senha</LoginButtonText>
-          </LoginButton>
-        </Form>
+            <ButtonSubmit
+              onPress={() => formRef.current?.submitForm()}
+              loading={loading}
+            >
+              Recuperar Senha
+            </ButtonSubmit>
+          </Form>
+        </FormProvider>
       </KeyboardAwareScrollView>
     </Container>
   );
--- src/screens/Not-Authenticated-Screens/RecoveryPassword/styles.ts
+++ src/screens/Not-Authenticated-Screens/RecoveryPassword/styles.ts
@@ -4,117 +4,42 @@ import {
 } from 'react-native-responsive-screen';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
+import Button from '@components/Button';
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
-  background: ${(props) => props.theme.colors.background};
-  flex-direction: column;
-`;
-
-export const I = styled.View`
-  padding-left: ${hp('2.5%')}px;
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  margin: 20px;
+  justify-content: space-around;
+  align-self: stretch;
 `;
 
 export const Header = styled.View`
-  height: ${hp('35%')}px;
+  margin: 20px 10px;
 
   justify-content: space-around;
 `;
-
 export const Form = styled.View`
-  height: ${hp('65%')}px;
-
+  justify-content: center;
   align-items: center;
-  justify-content: flex-start;
 `;
 
 export const Title = styled.Text`
   color: ${(props) => props.theme.colors.primary};
-  font-size: ${hp('5.5%')}px;
-  padding-left: ${hp('5%')}px;
-`;
-
-export const Input = styled.TextInput`
-  align-self: center;
-  background: #fff;
-  width: 85%;
-  height: ${hp('6.5%')}px;
-  elevation: 5;
-  border-radius: 22px;
-  margin-top: 26px;
-  padding-horizontal: ${hp('1.8%')}px;
-  padding-vertical: ${hp('1.8%')}px;
+  font-family: ${({ theme }) => theme.fonts.Ubuntu.bold};
+  font-size: 32px;
 `;
 
 export const P = styled.Text`
-  font-size: ${hp('2.4%')}px;
-  color: #666666;
-  padding-left: ${hp('5%')}px;
-  padding-right: ${hp('5%')}px;
+  padding-top: 5px;
+  color: ${(props) => props.theme.colors.subtitle};
+  font-family: ${({ theme }) => theme.fonts.Ubuntu.normal};
+  font-size: 15px;
 `;
 
-export const LoginButton = styled.TouchableOpacity`
-  width: ${wp('85%')}px;
-  border-radius: 22px;
-  height: ${hp('6%')}px;
-  margin-top: 25px;
-  margin-right: 25px;
-  margin-left: 25px;
-  justify-content: center;
-  background: ${(props) => props.theme.colors.primary};
-`;
-
-export const LoginButtonText = styled.Text`
-  text-align: center;
-  align-content: center;
-  font-size: ${hp('2.4%')}px;
-  color: ${(props) => props.theme.colors.secundary};
-`;
-
-export const RetrievePasswordButton = styled.TouchableOpacity`
-  border-radius: 22px;
-  height: ${hp('5%')}px;
-  margin-top: 25px;
-  margin-right: 25px;
-  margin-left: 25px;
-  justify-content: center;
-`;
-
-export const RetrievePasswordText = styled.Text`
-  text-align: center;
-  align-content: center;
-  font-size: ${hp('2.4%')}px;
-
-  color: ${(props) => props.theme.colors.primary};
-`;
-
-export const RegularText = styled.Text`
-  text-align: right;
-  align-content: center;
-  font-size: ${hp('2.4%')}px;
-  padding-top: ${hp('3%')}px;
-  width: ${wp('55%')}px;
-  color: #303030;
-`;
-
-export const RegisterButton = styled.TouchableOpacity`
-  border-radius: 22px;
-  height: ${hp('5%')}px;
-  padding-top: ${hp('2.4%')}px;
-  margin-right: 25px;
-  margin-left: 25px;
-
-  width: ${wp('40%')}px;
-`;
-
-export const RegisterButtonText = styled.Text`
-  text-align: left;
-  align-content: center;
-  font-size: ${hp('2.4%')}px;
-
-  color: ${(props) => props.theme.colors.primary};
-`;
-
-export const BackButtonWrapper = styled.TouchableOpacity`
-  padding-left: ${hp('2.5%')}px;
+export const ButtonSubmit = styled(Button)`
+  width: 100%;
+  max-width: 350px;
+  margin: 0 auto;
+  margin-top: 20px;
 `;
